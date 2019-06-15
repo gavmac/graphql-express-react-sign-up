01590aede5ee351b16688bff000f83b9dc125c77
gavmac <gavinmacken87@gmail.com>
2019-06-15 15:23:24 -0400

    model support

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
+import { Button, Container } from 'reactstrap';
 
 import PostViewer from './PostViewer';
+import PostEditor from './PostEditor';
 
 class App extends Component {
-  render() {
-    return (
-        <main>
-          <PostViewer />
-        </main>
-    );
-  }
+    state = {
+        editing: null,
+    };
+
+    render() {
+        const { editing } = this.state;
+
+        return (
+            <Container fluid>
+                <Button
+                    className="my-2"
+                    color="primary"
+                    onClick={() => this.setState({ editing: {} })}
+                >
+                    New Post
+                </Button>
+                <PostViewer
+                    canEdit={() => true}
+                    onEdit={(post) => this.setState({ editing: post })}
+                />
+                {editing && (
+                    <PostEditor
+                        post={editing}
+                        onClose={() => this.setState({ editing: null })}
+                    />
+                )}
+            </Container>
+        );
+    }
 }
 
 export default App;
