fc18fe2289bec2de340a2f083dd6ad8151aafce9
gavmac <gavinmacken87@gmail.com>
2019-06-20 09:55:14 -0400

    okta security log-in

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
-import { Button, Container } from 'reactstrap';
+import { Button, Container } from 'reactstrap'
+
 
 import PostViewer from './PostViewer';
 import PostEditor from './PostEditor';
+
+const config = {
+    issuer: '{process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default',
+    redirect_uri: window.location.origin + '/implicit/callback',
+    client_id:'{process.env.REACT_APP_OKTA_CLIENT_ID}'
+}
 
 class App extends Component {
     state = {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Security, ImplicitCallback } from '@okta/okta-react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import App from './App';
 import { ApolloProvider } from 'react-apollo';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -7,12 +10,21 @@ import client from './apollo';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-      <ApolloProvider client={client}>
-            <App />
-          </ApolloProvider>,
-      document.getElementById('root')
-    );
-if (module.hot) module.hot.accept();
+    <BrowserRouter>
+        <Security
+            issuer={`${process.env.REACT_APP_OKTA_ORG_URL}/oauth2/default`}
+            redirect_uri={`${window.location.origin}/implicit/callback`}
+            client_id={process.env.REACT_APP_OKTA_CLIENT_ID}
+        >
+            <ApolloProvider client={client}>
+                <Route path="/implicit/callback" component={ImplicitCallback} />
+                <Route path="/" component={App} />
+            </ApolloProvider>
+        </Security>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
+// if (module.hot) module.hot.accept();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
